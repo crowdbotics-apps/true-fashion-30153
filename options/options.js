58c79e4bd4279daa620106af95e6155f2e3efdaa
Crowdbotics <team@crowdbotics.com>
2021-08-27 22:41:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "true_fashion_30153Identifier",
+  url: "https://true_fashion_30153Identifier.botics.co",
+  api: "https://true_fashion_30153Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
